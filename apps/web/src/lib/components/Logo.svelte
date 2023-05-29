0dfc711b7aa0693ae0cc98bda5b9a7a64bee9813
Eunjae Lee <hey@eunjae.dev>
2023-05-29 19:41:22 +0200

    update skeleton

--- apps/web/src/lib/components/Logo.svelte
+++ apps/web/src/lib/components/Logo.svelte
@@ -1,7 +1,1 @@
-<script lang="ts">
-	import { APP_NAME } from '$lib/const';
-</script>
-
-<p class="unstyled text-primary-500 text-2xl font-semibold">
-	🍊 {APP_NAME}
-</p>
+<p>Logo</p>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import '../theme.postcss';
 
 	// This contains the bulk of Skeletons required styles:
-	import '@skeletonlabs/skeleton/styles/all.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
 
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
