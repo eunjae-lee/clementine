c87420d104b4c4080dda99fe62c5b39fbd8e7225
Eunjae Lee <hey@eunjae.dev>
2023-05-31 20:57:32 +0200

    add mdsvex support

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,15 +1,22 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import relativeImages from 'mdsvex-relative-images';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.svx'],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
 		vitePreprocess(),
 		preprocess({
 			postcss: true,
+		}),
+		mdsvex({
+			remarkPlugins: [relativeImages],
 		}),
 	],
 
